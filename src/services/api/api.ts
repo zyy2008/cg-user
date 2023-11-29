338e0ae834e788e69cbb09111dbb7a0ef6834fed
zyy <zyy2008@vip.qq.com>
2023-11-29 16:18:37 +0800

    fix: 提交

---
+++ src/services/api/api.ts
@@ -0,0 +1,9 @@
+export * from './SysDeptControllerApi';
+import SysDeptControllerApi from './SysDeptControllerApi';
+export * from './SysMenuControllerApi';
+import SysMenuControllerApi from './SysMenuControllerApi';
+export * from './SysRoleControllerApi';
+import SysRoleControllerApi from './SysRoleControllerApi';
+export * from './SysUserControllerApi';
+import SysUserControllerApi from './SysUserControllerApi';
+export const APIS = { SysDeptControllerApi, SysMenuControllerApi, SysRoleControllerApi, SysUserControllerApi };
---
+++ src/services/model/models.ts
@@ -0,0 +1,15 @@
+export * from './LoginUser';
+export * from './RLoginUser';
+export * from './Rboolean';
+export * from './SysDept';
+export * from './SysMenu';
+export * from './SysRole';
+export * from './SysRoleReq';
+export * from './SysRoleRes';
+export * from './SysUserReq';
+export * from './SysUserRes';
+export * from './SysUserRole';
+export * from './TableDataInfo';
+export type ModelInteger = number;
+export type ModelFloat = number;
+export type LocalDate = Date;
